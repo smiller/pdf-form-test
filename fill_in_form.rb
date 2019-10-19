2cd12afa6e923d41bf604a23a3c8ae0971c6c9d8
Sean Miller <sean@seanmiller.ca>
2019-10-19 14:05:47 -0400

    Fix try 2: move [:NeedAppearances] line up

--- fill_in_form.rb
+++ fill_in_form.rb
@@ -1,24 +1,25 @@
 require 'hexapdf'
 
+# Get empty form
 template_name = "./SimpleForm.pdf"
-
 pdf = HexaPDF::Document.open(template_name)
 
+# Set field value
 field = pdf.object(22)
 field[:V] = "2019-09-09"
 field[:Ff] ||= 0
 field[:Ff] |= (1 << 0) # readonly
 
+# Set NeedAppearances, required for Acrobat
+pdf.catalog[:AcroForm][:NeedAppearances] = true
+
+# Save form with field and NeedAppearances
 filled_in_name = "./FilledInForm.pdf" 
 pdf.write(filled_in_name)
 
+# Save optimized version
 optimized_name = "./FilledInFormOptimized.pdf" 
-
-package = HexaPDF::Document.new
 pdf = HexaPDF::Document.open(filled_in_name)
-pdf.catalog[:AcroForm][:NeedAppearances] = true
-pdf.pages.each { |page| package.pages << package.import(page) }
-package.task(:optimize, compact: true,
-                        object_streams: :delete)
-
-package.write(optimized_name)
+pdf.task(:optimize, compact: true,
+                    object_streams: :delete)
+pdf.write(optimized_name)
