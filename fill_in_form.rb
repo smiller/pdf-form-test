90937feaf7fa4b8db7ac94b72bb480f88e163c04
Sean Miller <sean@seanmiller.ca>
2019-10-19 14:39:06 -0400

    Try NeedAppearance fix with multiple input files
    Initial try: just apply NeedApearance fix
    to the individual files, then combine them.
    
    Doesn't work: 'CombinedForms.pdf' is back to the old
    field doesn't display in Acrobat until you click
    within the field behaviour.

--- fill_in_form.rb
+++ fill_in_form.rb
@@ -1,25 +1,64 @@
 require 'hexapdf'
 
-# Get empty form
-template_name = "./SimpleForm.pdf"
-pdf = HexaPDF::Document.open(template_name)
+# SimpleForm1
 
-# Set field value
+## Get empty form
+template_name1 = "./SimpleForm1.pdf"
+pdf = HexaPDF::Document.open(template_name1)
+
+## Set field value
 field = pdf.object(22)
-field[:V] = "2019-09-09"
+field[:V] = "2019-01-01"
 field[:Ff] ||= 0
 field[:Ff] |= (1 << 0) # readonly
 
-# Set NeedAppearances, required for Acrobat
+## Set NeedAppearances, required for Acrobat
 pdf.catalog[:AcroForm][:NeedAppearances] = true
 
-# Save form with field and NeedAppearances
-filled_in_name = "./FilledInForm.pdf" 
-pdf.write(filled_in_name)
+## Save form with field and NeedAppearances
+filled_in_name1 = "./FilledInForm1.pdf" 
+pdf.write(filled_in_name1)
 
-# Save optimized version
-optimized_name = "./FilledInFormOptimized.pdf" 
-pdf = HexaPDF::Document.open(filled_in_name)
+## Save optimized version
+optimized_name1 = "./FilledInForm1Optimized.pdf" 
+pdf = HexaPDF::Document.open(filled_in_name1)
 pdf.task(:optimize, compact: true,
                     object_streams: :delete)
-pdf.write(optimized_name)
+pdf.write(optimized_name1)
+
+# SimpleForm2
+
+## Get empty form
+template_name2 = "./SimpleForm2.pdf"
+pdf = HexaPDF::Document.open(template_name2)
+
+## Set field value
+field = pdf.object(22)
+field[:V] = "2019-02-02"
+field[:Ff] ||= 0
+field[:Ff] |= (1 << 0) # readonly
+
+## Set NeedAppearances, required for Acrobat
+pdf.catalog[:AcroForm][:NeedAppearances] = true
+
+## Save form with field and NeedAppearances
+filled_in_name2 = "./FilledInForm2.pdf" 
+pdf.write(filled_in_name2)
+
+## Save optimized version
+optimized_name2 = "./FilledInForm2Optimized.pdf" 
+pdf = HexaPDF::Document.open(filled_in_name2)
+pdf.task(:optimize, compact: true,
+                    object_streams: :delete)
+pdf.write(optimized_name2)
+
+# Combine forms
+package_name = "./CombinedForms.pdf"
+package = HexaPDF::Document.new
+
+[optimized_name1, optimized_name2].each do |name|
+  pdf = HexaPDF::Document.open(name)
+  pdf.pages.each { |page| package.pages << package.import(page) }
+end
+
+package.write(package_name)
